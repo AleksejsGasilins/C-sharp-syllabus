202cea5e925d482a1decda5c2818a8ca3e9e7965
Aleksejs <aleksejsgasilins@gmail.com>
2021-11-25 18:00:50 +0200

    Exercise-7 done.

---
+++ csharp-basics/exercises/Arithmetic/Exercise-7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Linq;
+
+namespace Exercise_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 110; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.Write(" Coza ");
+                } else if (i % 5 == 0)
+                {
+                    Console.Write(" Loza ");
+                } else if (i % 7 == 0)
+                {
+                    Console.Write(" Woza ");
+                } else if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.Write(" CozaLoza ");
+                }
+                else
+                {
+                    Console.Write(" " + i + " ");
+                }
+
+                if (i % 11 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
