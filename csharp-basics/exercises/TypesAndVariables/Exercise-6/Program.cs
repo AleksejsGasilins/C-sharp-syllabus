c057cf57dde0b05acfeb6370fa10fe0e120c8c34
Aleksejs <aleksejsgasilins@gmail.com>
2021-11-30 01:24:26 +0200

    Exercise-6 done.

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise-6/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Exercise_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 0;
+            string data;
+
+            Console.WriteLine("Hi, what numbers do you want to add? "); 
+            data = Console.ReadLine();
+
+            if (int.TryParse(data, out var parsedNumber))
+            {
+                x = Convert.ToInt32(data);
+
+                Console.WriteLine("Second number");
+            }
+            else
+            {
+                Console.WriteLine("Not suitable symbol");
+                Console.ReadKey();
+            }
+
+            data = Console.ReadLine();
+
+            if (int.TryParse(data, out var parsedNumber2))
+            {
+                y = Convert.ToInt32(data);
+
+                Console.WriteLine("Answer ");
+                Console.WriteLine(x + y);
+            }
+            else
+            {
+                Console.WriteLine("Not suitable symbol");
+                Console.ReadKey();
+            }
+
+
+
+        }
+    }
+}
