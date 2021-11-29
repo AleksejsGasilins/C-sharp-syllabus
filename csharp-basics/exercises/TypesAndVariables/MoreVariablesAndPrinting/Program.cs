0a07c6944f6ccccb19b57504c0ea1e63c182a85b
Aleksejs <aleksejsgasilins@gmail.com>
2021-11-29 19:32:44 +0200

    Exercise-2 done.

--- csharp-basics/exercises/TypesAndVariables/MoreVariablesAndPrinting/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/MoreVariablesAndPrinting/Program.cs
@@ -6,26 +6,35 @@ namespace MoreVariablesAndPrinting
     {
         static void Main(string[] args)
         {
-            string myName, myEyes, myTeeth, myHair;
-            int myAge, myHeight, myWeight;
+            string Name;
+            string Eyes;
+            string Teeth; 
+            string Hair;
+            int Age;
+            int Height; 
+            int Weight;
+            decimal HeightInCm;
+            decimal WeightInKg;
 
-            myName = "Zed A. Shaw";
-            myAge = 35;
-            myHeight = 74;  // inches
-            myWeight = 180; // lbs
-            myEyes = "Blue";
-            myTeeth = "White";
-            myHair = "Brown";
+            Name = "Zed A. Shaw";
+            Age = 35;
+            Height = 74;  // inches
+            Weight = 180; // lbs
+            Eyes = "Blue";
+            Teeth = "White";
+            Hair = "Brown";
+            HeightInCm = Math.Round((Height * 2.54m), 2);
+            WeightInKg = Math.Round((Weight * 0.453592m), 2);
 
-            Console.WriteLine("Let's talk about " + myName + ".");
-            Console.WriteLine("He's " + myHeight + " inches tall.");
-            Console.WriteLine("He's " + myWeight + " pounds heavy.");
+            Console.WriteLine("Let's talk about " + Name + ".");
+            Console.WriteLine("He's " + HeightInCm + " inches tall.");
+            Console.WriteLine("He's " + WeightInKg + " pounds heavy.");
             Console.WriteLine("Actually, that's not too heavy.");
-            Console.WriteLine("He's got " + myEyes + " eyes and " + myHair + " hair.");
-            Console.WriteLine("His teeth are usually " + myTeeth + " depending on the coffee.");
+            Console.WriteLine("He's got " + Eyes + " eyes and " + Hair + " hair.");
+            Console.WriteLine("His teeth are usually " + Teeth + " depending on the coffee.");
 
-            Console.WriteLine("If I add " + myAge + ", " + myHeight + ", and " + myWeight
-                               + " I get " + (myAge + myHeight + myWeight) + ".");
+            Console.WriteLine("If I add " + Age + ", " + HeightInCm + ", and " + WeightInKg
+                               + " I get " + (Age + HeightInCm + WeightInKg) + ".");
 
             Console.ReadKey();
         }
