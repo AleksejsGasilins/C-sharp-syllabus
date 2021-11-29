d9b3eaa83c84316692bed217c3e5e4ebfb420da3
Aleksejs <aleksejsgasilins@gmail.com>
2021-11-29 20:17:06 +0200

    Exercise-4 done.

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise-4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Juan";
+            string MyLastName = "Valdez";
+            int MyBornData = 1995;
+
+            Console.WriteLine("My name is " + MyName + " " + MyLastName + " and I was born in " + MyBornData + ".");
+            Console.ReadKey();
+        }
+    }
+}
