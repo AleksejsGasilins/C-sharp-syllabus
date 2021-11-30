09e1eb98e0a449a1813bea6ac5e7c59d6875e1b9
Aleksejs <aleksejsgasilins@gmail.com>
2021-11-30 16:06:01 +0200

    Exercise-7 done.

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise-7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Microsoft.VisualBasic;
+
+namespace Exercise_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string AmountOfSymbols;
+            char[] UserText;
+            int upper = 0;
+
+            Console.WriteLine("Do you want to know the number of Uppercase letters in the text?");
+            AmountOfSymbols = Console.ReadLine();
+            UserText = AmountOfSymbols.ToCharArray();
+
+            for (int i = 0; i < UserText.Length; i++)
+            {
+                if (char.IsUpper(UserText[i]))
+                {
+                    upper++;
+                }
+            }
+
+            Console.WriteLine("Total Upper Case Letters: ");
+            Console.WriteLine(upper.ToString());
+        }
+    }
+}
