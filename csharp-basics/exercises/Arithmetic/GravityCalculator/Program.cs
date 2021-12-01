6a51f48710e3af54609de09acacd388b456b0cfd
Aleksejs <aleksejsgasilins@gmail.com>
2021-12-01 02:34:39 +0200

    Exercise fix.

--- csharp-basics/exercises/Arithmetic/GravityCalculator/Program.cs
+++ csharp-basics/exercises/Arithmetic/GravityCalculator/Program.cs
@@ -6,7 +6,7 @@ namespace GravityCalculator
     {
         static void Main(string[] args)
         {
-            double gravity = -9.81d;  // Earth's gravity in m/s^2
+            double gravity = -9.81d;
             double initialVelocity = 0.0d;
             double fallingTime = 10.0d;
             double initialPosition = 0.0d;
@@ -15,7 +15,6 @@ namespace GravityCalculator
             finalPosition = 0.5 * gravity * (fallingTime * fallingTime) + initialVelocity * fallingTime + initialPosition;
             Convert.ToDecimal(finalPosition);
             
-
             Console.WriteLine("The object's position after " + fallingTime + " seconds is " + finalPosition + " m.");
             Console.ReadKey();
         }
