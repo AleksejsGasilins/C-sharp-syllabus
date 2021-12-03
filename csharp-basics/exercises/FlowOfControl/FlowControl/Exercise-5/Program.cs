84434e4a42aacb987bbeb1bdb35c653bcc025371
Aleksejs <aleksejsgasilins@gmail.com>
2021-12-03 23:05:01 +0200

    Exercise-5 done.

--- csharp-basics/exercises/FlowOfControl/FlowControl/Exercise-5/Program.cs
+++ csharp-basics/exercises/FlowOfControl/FlowControl/Exercise-5/Program.cs
@@ -6,7 +6,48 @@ namespace Exercise_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Write your text: ");
+            string userEnter = Console.ReadLine();
+
+            string allReadyStr;
+            char[] allTrim;
+
+            allReadyStr = userEnter.ToLower();
+            allTrim = allReadyStr.ToCharArray();
+
+            for (int i = 0; i < allTrim.Length; i++)
+            {
+                switch (allTrim[i])
+                {
+                    case 'a':case 'b': case 'c':
+                        Console.Write(2);
+                        break;
+                    case 'd': case 'e': case 'f':
+                        Console.Write(3);
+                        break;
+                    case 'g': case 'h': case 'i':
+                        Console.Write(4);
+                        break;
+                    case 'j': case 'k': case 'l':
+                        Console.Write(5);
+                        break;
+                    case 'm': case 'n': case 'o':
+                        Console.Write(6);
+                        break;
+                    case 'p': case 'q': case 'r': case 's':
+                        Console.Write(7);
+                        break;
+                    case 't': case 'u':  case 'v':
+                        Console.Write(8);
+                        break;
+                    case 'w': case 'x': case 'y': case 'z':
+                        Console.Write(9);
+                        break;
+                }
+            }
+
+            Console.WriteLine("Good by!");
+            Console.ReadKey();
         }
     }
 }
