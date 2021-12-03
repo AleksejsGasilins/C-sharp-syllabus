aee87878dddbe4f4aa8327f80db5de443a6334d9
Aleksejs <aleksejsgasilins@gmail.com>
2021-12-03 17:54:56 +0200

    Exercise-4 done.

---
+++ csharp-basics/exercises/FlowOfControl/FlowControl/PrintDayInWord/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrintDayInWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dayNumber;
+
+            Console.WriteLine("Write the number: ");
+            ConsoleKeyInfo userEnter = Console.ReadKey();
+
+            if (char.IsDigit(userEnter.KeyChar))
+            {
+                dayNumber = int.Parse(userEnter.KeyChar.ToString());
+            }
+            else
+            {
+                dayNumber = -1;
+            }
+
+            switch (dayNumber)
+                {
+                    case 0:
+                        Console.WriteLine("\n" + "Today is Monday!");
+                        break;
+                    case 1:
+                        Console.WriteLine("\n" + "Today is Tuesday !");
+                        break;
+                    case 2:
+                        Console.WriteLine("\n" + "Today is Wednesday!");
+                        break;
+                    case 3:
+                        Console.WriteLine("\n" + "Today is Thursday!");
+                        break;
+                    case 4:
+                        Console.WriteLine("\n" + "Today is Friday!");
+                        break;
+                    case 5:
+                        Console.WriteLine("\n" + "Today is Saturday!");
+                        break;
+                    case 6:
+                        Console.WriteLine("\n" + "Today is Sunday!");
+                        break;
+                    default:
+                        Console.WriteLine("\n" + "Not a valid day!");
+                        break;
+            }
+        }
+    }
+}
