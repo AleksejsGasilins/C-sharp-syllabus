aa45aac264b2c28540124fdd943fb77acd54e61b
Aleksejs <aleksejsgasilins@gmail.com>
2021-12-08 03:04:07 +0200

    Exercise-3 in progress.

---
+++ csharp-basics/exercises/Loops/Loops/Exercise-3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[20];
+            Random rndNum = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                nums[i] = rndNum.Next(1, 100);
+                Console.WriteLine(i + "-" + nums[i] + " ");
+            }
+
+            foreach (int index in nums)
+            {
+                Console.Write(nums[7]);
+            }
+
+        }
+    }
+}
