8b2c9b107386ba72eb10ed3451e1049d90eaa78b
Aleksejs <aleksejsgasilins@gmail.com>
2021-12-08 21:47:59 +0200

    Exercise-3 done.

--- csharp-basics/exercises/Loops/Loops/Exercise-3/Program.cs
+++ csharp-basics/exercises/Loops/Loops/Exercise-3/Program.cs
@@ -8,17 +8,23 @@ namespace Exercise_3
         {
             int[] nums = new int[20];
             Random rndNum = new Random();
+
             for (int i = 0; i < 20; i++)
             {
                 nums[i] = rndNum.Next(1, 100);
                 Console.WriteLine(i + "-" + nums[i] + " ");
-            }
 
-            foreach (int index in nums)
-            {
-                Console.Write(nums[7]);
-            }
+                if (i == 20 - 1)
+                {
+                    Console.WriteLine("Enter the position number you want to receive: ");
+                    var userNumPosition = Console.ReadLine();
+                    int userNumInInt = Convert.ToInt32(userNumPosition);
 
+                    Console.WriteLine("Here is your number: ");
+                    Console.WriteLine(nums[userNumInInt]);
+                    Console.ReadKey();
+                }
+            }
         }
     }
 }
