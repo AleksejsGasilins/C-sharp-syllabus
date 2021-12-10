c6a32a9c95820f610f12eb2eb9516f5eaa4ffc92
Aleksejs <aleksejsgasilins@gmail.com>
2021-12-10 21:35:08 +0200

    Exercise-6 done.

---
+++ csharp-basics/exercises/Loops/Loops/Exercise-6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exercise_6
+{
+    class FizzBuzz
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the max number: ");
+            string userNum = Console.ReadLine();
+            int userNumInInt = Convert.ToInt32(userNum);
+
+            for (int i = 1; i < userNumInInt + 1; i++)
+            {
+                if (i % 21 == 0)
+                {
+                    Console.WriteLine();
+                }
+                if (i % 3 == 0)
+                {
+                    Console.Write("Fizz ");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.Write("Buzz ");
+                }
+                else if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.Write("FizzBuzz ");
+                }
+                else
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
