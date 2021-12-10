e5ce761abf4d821f2ee2fe2b8645078b2fb252f2
Aleksejs <aleksejsgasilins@gmail.com>
2021-12-10 22:27:54 +0200

    Exercise-7 in progress.

---
+++ csharp-basics/exercises/Loops/Loops/Exercise-7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercise_7
+{
+    class Piglet
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Piglet!");
+            Console.WriteLine("Do you wanna start game? Press any button!");
+            Console.ReadKey();
+
+            Random rnd = new Random();
+            int getRndNum = rnd.Next(1, 6);
+            int firstRndNum = rnd.Next(2, 6);
+
+            Console.WriteLine("You rolled a " + firstRndNum + "!");
+            Console.Write("Roll again? (Y/N)");
+            string userChoice = Console.ReadLine();
+
+            int userScore = 0;
+
+            for (int i = getRndNum; i != 1; i++)
+            {
+                
+            }
+        }
+    }
+}
