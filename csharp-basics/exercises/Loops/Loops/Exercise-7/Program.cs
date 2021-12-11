2c1eb8460b63bb281e4d3446e36c153cbcc2ec51
Aleksejs <aleksejsgasilins@gmail.com>
2021-12-11 22:18:31 +0200

    Exercise-7 done.

--- csharp-basics/exercises/Loops/Loops/Exercise-7/Program.cs
+++ csharp-basics/exercises/Loops/Loops/Exercise-7/Program.cs
@@ -11,18 +11,50 @@ namespace Exercise_7
             Console.ReadKey();
 
             Random rnd = new Random();
-            int getRndNum = rnd.Next(1, 6);
             int firstRndNum = rnd.Next(2, 6);
 
+            int userScore = 0;
+            int loseScore = 0;
+
             Console.WriteLine("You rolled a " + firstRndNum + "!");
-            Console.Write("Roll again? (Y/N)");
+            userScore += firstRndNum;
+
+            Console.Write("Roll again? (Y/N): ");
             string userChoice = Console.ReadLine();
 
-            int userScore = 0;
-
-            for (int i = getRndNum; i != 1; i++)
+            for (int i = firstRndNum; i != 1; i++)
             {
-                
+                int secondRndNum = rnd.Next(1, 6);
+
+                if (secondRndNum != 1)
+                {
+                    if (userChoice == userChoice)
+                    {
+                        if (userChoice.ToLower() == "yes" || userChoice.ToLower() == "y")
+                        {
+                            Console.WriteLine("You rolled a " + secondRndNum + "!");
+                            userScore += secondRndNum;
+
+                            Console.Write("Roll again? (Y/N): ");
+                            userChoice = Console.ReadLine();
+                        }
+                        else if (userChoice.ToLower() == "no" || userChoice.ToLower() == "n")
+                        {
+                            Console.WriteLine($"You got {userScore} points.");
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"You rolled a {secondRndNum}.");
+                    Console.WriteLine($"You got {loseScore} points.");
+                    break;
+                }
             }
         }
     }
