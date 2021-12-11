21fa3e63b6cfc000e9768fa1315d42c2ed21c46c
Aleksejs <aleksejsgasilins@gmail.com>
2021-12-11 23:54:15 +0200

    Exercise-8 in progress.

---
+++ csharp-basics/exercises/Loops/Loops/Exercise-8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise_8
+{
+    class AsciiFigure
+    {
+        static void Main(string[] args)
+        {
+            DrawFunction(3);
+        }
+
+        static void DrawFunction(int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                string star = string.Empty; // use string
+                for (int counter = 0; counter < 10; counter++)
+                {
+                    star += "*"; // append a star
+                    Console.WriteLine(star);
+                }
+            }
+        }
+    }
+}
