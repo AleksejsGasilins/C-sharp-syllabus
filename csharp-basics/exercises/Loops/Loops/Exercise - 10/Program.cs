28bfc2933e4003267de76a9adfe5b2cfd53c6417
Aleksejs <aleksejsgasilins@gmail.com>
2021-12-12 04:48:49 +0200

    Exercise - 10 in progress.

---
+++ csharp-basics/exercises/Loops/Loops/Exercise - 10/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exercise___10
+{
+    class NumberSquare
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter min number: ");
+            var userMinNum = Console.ReadKey();
+
+            Console.Write("\nEnter max number: ");
+            var userMaxNum = Console.ReadKey();
+
+            Console.WriteLine();
+
+            int minNumToInt = Сheck(userMinNum);
+            int maxBumToInt = Сheck(userMaxNum);
+            string count = " ";
+
+            for (int i = minNumToInt; i != maxBumToInt + 1; i++)
+            {
+                Console.Write(i);
+            }
+        }
+
+        public static int Сheck(ConsoleKeyInfo UserInput)
+        {
+            int Bowl;
+
+            if (char.IsDigit(UserInput.KeyChar))
+            {
+               return Bowl = int.Parse(UserInput.KeyChar.ToString());
+            }
+            else
+            {
+               return Bowl = -1;
+            }
+        }
+    }
+}
