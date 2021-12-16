706e0655cf1082e9726ee4dfe0317756e9d66753
Aleksejs <aleksejsgasilins@gmail.com>
2021-12-17 00:18:41 +0200

    Exercise-5 done.

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise-5/Data.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_5
+{
+    public class Data
+    {
+        private int _month;
+        private int _day;
+        private int _year;
+
+        public Data(int month,int day,int year)
+        {
+            _month = month;
+            _day = day;
+            _year = year;
+        }
+
+        public int GetMonth()
+        {
+            return _month;
+        }
+
+        public int GetDay()
+        {
+            return _day;
+        }
+
+        public int GetYear()
+        {
+            return _year;
+        }
+
+        public void SetMonth(int month)
+        {
+            _month = month;
+        }
+
+        public void SetDay(int day)
+        {
+            _day = day;
+        }
+
+        public void SetYear(int year)
+        {
+            _year = year;
+        }
+
+        public void DisplayDate()
+        {
+            Console.Write($"{GetMonth()}/{GetDay()}/{GetYear()}\n");
+        }
+    }
+}
