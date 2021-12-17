d2cc6a258701a255d760cf66b086d041a352b58f
Aleksejs <aleksejsgasilins@gmail.com>
2021-12-18 01:21:13 +0200

    Exercise-4 done.

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise-4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var casinoRoyale = new Movie("Casino Royale", "Eon Productions", "PG13");
+            var glass = new Movie("Glass", "Buena Vista International", "PG13");
+            var spiderMan = new Movie("Spider-Man: Into the Spider-Verse", "Columbia Pictures", "PG");
+            var movies = new List<Movie>
+            {
+                casinoRoyale,
+                glass,
+                spiderMan
+            };
+
+            var filtered = GetPg(movies.ToArray());
+            foreach (var movie in filtered)
+            {
+                Console.WriteLine($"{movie}");
+                Console.WriteLine($"{movie.Display()}");
+            }
+        }
+
+        public static Movie[] GetPg(Movie[] movies)
+        {
+            var filtered = new List<Movie>();
+
+            foreach (var movie in movies)
+            {
+                if (movie.Rating == "PG")
+                {
+                    filtered.Add(movie);
+                }
+            }
+
+            return filtered.ToArray();
+        }
+    }
+}
