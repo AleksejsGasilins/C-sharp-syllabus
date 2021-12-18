f6f0100b2e762817561fd7c73b5ea64965a82e7f
Aleksejs <aleksejsgasilins@gmail.com>
2021-12-18 20:56:01 +0200

    Exercise-9 done.

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise-9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Drawing;
+
+namespace Exercise_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Point(5, 2);
+            var p2 = new Point(-3, 6);
+
+            Console.WriteLine(p1.getPoint());
+            Console.WriteLine(p2.getPoint());
+
+            p1.SwapPoints(p2);
+
+            Console.WriteLine(p1.getPoint());
+            Console.WriteLine(p2.getPoint());
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise-9/SwapPoints.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_9
+{
+    class Point
+    {
+        private int _x;
+        private int _y;
+
+        public Point(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public void SwapPoints(Point p2)
+        {
+            (_x, p2._x) = (p2._x, _x);
+            (_y, p2._y) = (p2._y, _y);
+        }
+
+        public string getPoint()
+        {
+            return $"{_x}, {_y}";
+        }
+    }
+}
