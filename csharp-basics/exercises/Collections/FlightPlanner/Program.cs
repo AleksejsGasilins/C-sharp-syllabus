719f32cd07588b9c7c004c918d48633446c9ef29
Aleksejs <aleksejsgasilins@gmail.com>
2021-12-23 01:42:15 +0200

    Exercise-6 in progress.

--- csharp-basics/exercises/Collections/FlightPlanner/Program.cs
+++ csharp-basics/exercises/Collections/FlightPlanner/Program.cs
@@ -13,11 +13,47 @@ namespace FlightPlanner
 
         private static void Main(string[] args)
         {
-            var readText = File.ReadAllLines(Path);
-            foreach (var s in readText)
+            List<string> cities = new List<string>()
             {
-                Console.WriteLine(s);
+                "San Jose",
+                "San Francisco",
+                "Anchorage",
+                "New York",
+                "Honolulu",
+                "Denver"
+            };
+
+            ConsoleKeyInfo userChoice;
+
+            do
+            {
+                Console.WriteLine("What would you like to do: ");
+                Console.WriteLine("To display list of the cities press 1");
+                Console.WriteLine("To exit program press #");
+
+                userChoice = Console.ReadKey();
+
+
+            } while (userChoice.ToString() == "1" || userChoice.ToString() == "#");
+
+            if (userChoice.ToString() == "1")
+            {
+                Console.WriteLine("All Cities: ");
+                foreach (var city in cities)
+                {
+                    Console.WriteLine(city);
+                }
             }
+
+
+
+
+            //var readText = File.ReadAllLines(Path);
+            //foreach (var s in readText)
+            //{
+            //    Console.WriteLine(s);
+            //}
+            Console.ReadKey();
         }
     }
 }
