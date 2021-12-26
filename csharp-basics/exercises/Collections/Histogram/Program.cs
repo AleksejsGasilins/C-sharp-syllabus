60e155d44679b257c8a392aab5e9ebc33b000ff7
Aleksejs <aleksejsgasilins@gmail.com>
2021-12-27 00:44:30 +0200

    Exercise-7 done

--- csharp-basics/exercises/Collections/Histogram/Program.cs
+++ csharp-basics/exercises/Collections/Histogram/Program.cs
@@ -9,15 +9,59 @@ namespace Histogram
 {
     class Program
     {
-        private const string Path = "../Histogram/midtermscores.txt";
+        private const string Path = @"E:\Programming\Studying\C-sharp-syllabus\csharp-basics\exercises\Collections\Histogram\midtermscores.txt";
 
         private static void Main(string[] args)
         {
-            var readText = File.ReadAllLines(Path);
-            foreach (var s in readText)
+            var numsList = СountingLikes.GetNumbers(Path);
+
+            var min = 0;
+            var max = 9;
+
+            for (int i = 1; i <= 11; i++)
             {
-                Console.WriteLine(s);
+                var stars = "";
+                var count = 0;
+
+                foreach (var num in numsList)
+                {
+                    if (num >= min && num <= max)
+                    {
+                        count += 1;
+                    }
+                }
+
+                for (int j = 0; j < count; j++)
+                {
+                    stars += "*";
+                }
+
+                if (i == 1)
+                {
+                    Console.WriteLine($"0{min}-0{max}: {stars}");
+                }
+                else if (i == 11)
+                {
+                    Console.WriteLine($"  {max}: {stars}");
+                }
+                else
+                {
+                    Console.WriteLine($"{min}-{max}: {stars}");
+                }
+
+                if (i <= 9)
+                {
+                    min += 10;
+                    max += 10;
+                }
+                else
+                {
+                    min += 10;
+                    max += 1;
+                }
             }
+
+            Console.ReadKey();
         }
     }
 }
