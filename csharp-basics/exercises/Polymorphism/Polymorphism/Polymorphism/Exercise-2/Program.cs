f8306be1ddf5d9d6428900aaac2dee7dde9374ee
Aleksejs <aleksejsgasilins@gmail.com>
2021-12-30 16:00:40 +0200

    Exercise-2 done.

---
+++ csharp-basics/exercises/Polymorphism/Polymorphism/Polymorphism/Exercise-2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MakeSounds
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var sounds = new List<ISound>();
+            sounds.Add(new Firework());
+            sounds.Add(new Radio());
+            sounds.Add(new Parrot());
+
+            Console.WriteLine("Choice sound: ");
+            Console.WriteLine("1. Firework");
+            Console.WriteLine("2. Radio");
+            Console.WriteLine("3. Parrot");
+            Console.Write("Your number is: ");
+            var userInput = Convert.ToInt32(Console.ReadLine());
+
+            switch (userInput)
+            {
+                case 1:
+                    Console.WriteLine(sounds[0].PlaySound());
+                    break;
+                case 2:
+                    Console.WriteLine(sounds[1].PlaySound());
+                    break;
+                case 3:
+                    Console.WriteLine(sounds[2].PlaySound());
+                    break;
+            }
+        }
+    }
+}
---
+++ csharp-basics/exercises/Polymorphism/Polymorphism/Polymorphism/Exercise-2/Radio.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MakeSounds
+{
+    class Radio : ISound
+    {
+
+        public string PlaySound()
+        {
+          return "somebody once told me...";
+        }
+    }
+}
