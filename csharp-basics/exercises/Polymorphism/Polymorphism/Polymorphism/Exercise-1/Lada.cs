6f21ad1f3140a41f6e47844e49660b6e06f5acaf
Aleksejs <aleksejsgasilins@gmail.com>
2021-12-30 14:35:19 +0200

    Exercise-1 done.

---
+++ csharp-basics/exercises/Polymorphism/Polymorphism/Polymorphism/Exercise-1/Lada.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragRace
+{
+    public class Lada : ICar
+    {
+        private int _currentSpeed = 0;
+        public void SpeedUp()
+        {
+            _currentSpeed += 50;
+        }
+
+        public void SlowDown()
+        {
+            _currentSpeed -= 50;
+        }
+
+        public string ShowCurrentSpeed()
+        {
+            return _currentSpeed.ToString();
+        }
+
+        public void StartEngine()
+        {
+            Console.WriteLine("--- rum rum rum ---");
+        }
+    }
+}
